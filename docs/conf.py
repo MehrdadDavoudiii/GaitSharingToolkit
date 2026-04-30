4b254252d85e4b784fa943c8fb809c0b96d13cd7
Mehrdad <mehrdaddavoodi15@gmail.com>
2026-04-30 14:38:58 +0200

    docs

--- docs/conf.py
+++ docs/conf.py
@@ -19,8 +19,8 @@ exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 html_theme = "sphinx_rtd_theme"
 html_static_path = ["_static"]
 html_css_files = ["css/custom.css"]
-html_logo = "_static/images/logo.png"
-html_favicon = "_static/images/logo.png"
+# html_logo = "_static/images/logo.png"
+# html_favicon = "_static/images/logo.png"
 
 html_theme_options = {
     "logo_only": False,
